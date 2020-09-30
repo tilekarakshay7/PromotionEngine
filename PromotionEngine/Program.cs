a139c577e71505961503d11c4a9603ede92ed6eb
Akshay Tilekar <akshaytilekar7@gmail.com>
2020-09-30 21:58:40 +0530

    finally testes from client side, ie, from programe.cs (All pdf scenarioes)

--- PromotionEngine/Program.cs
+++ PromotionEngine/Program.cs
@@ -1,15 +1,51 @@
-﻿using System;
+﻿using PromotionEngine.Helpers;
+using PromotionEngine.Models;
+using PromotionEngine.Services;
+using PromotionEngine.Services.Interfaces;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PromotionEngine
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            IPromotionRuleServices PromotionRules = new PromotionRuleServices();
+            IItemServices itemServices = new ItemServices();
+            IOrderServices orderServices = new OrderServices(PromotionRules, itemServices);
+
+
+            #region 3_A_FOR_130
+            List<Cart> ListOfCartItemDetails1 = new List<Cart>()
+            {
+                new Cart(Constants.A, 3)
+            };
+
+            var total = orderServices.ProcessBill(ListOfCartItemDetails1);
+            Console.WriteLine(total);
+            #endregion
+
+            #region 2_B_For_45
+            List<Cart> ListOfCartItemDetails2 = new List<Cart>()
+            {
+                new Cart(Constants.B, 2)
+            };
+
+            total = orderServices.ProcessBill(ListOfCartItemDetails2);
+            Console.WriteLine(total);
+            #endregion
+
+            #region C_AND_D_FOR_30
+            List<Cart> ListOfCartItemDetails3 = new List<Cart>()
+            {
+                new Cart(Constants.C, 1),
+                new Cart(Constants.D, 1)
+            };
+
+            total = orderServices.ProcessBill(ListOfCartItemDetails3);
+            Console.WriteLine(total);
+            #endregion
         }
     }
 }
