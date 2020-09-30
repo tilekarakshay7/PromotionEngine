7d5ffbd89438580b0e369568a9d88ebb52faa62e
Akshay Tilekar <akshaytilekar7@gmail.com>
2020-09-30 19:17:17 +0530

    added entity for PromotionRule

---
+++ PromotionEngine/Models/PromotionRule.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace PromotionEngine.Models
+{
+    public class PromotionRule
+    {
+        public string SKUId { get; set; }
+
+        public string RuleName { get; set; }
+
+        public int NumberOfApperance { get; set; }
+
+        public int PercentageToReduceFromPrice { get; set; }
+
+        public int LumsumAmountToReduceFromPrice { get; set; }
+
+        public List<string> ListOfAnotherItemsToBeConsidered { get; set; }
+    }
+}
